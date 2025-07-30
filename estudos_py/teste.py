445df4843b389b05c991d89f1bea909417d35c30
Eduardo Umbelino <umbelino.eduardo2003@gmail.com>
2025-07-30 00:35:13 -0300

    base do comando print em py
    lembrando comando em py com input e fazendo contas Transformando o inpt em int e podendo fazer contas

---
+++ estudos_py/teste.py
@@ -0,0 +1,17 @@
+nome00 = input('Digite seu primeiro nome: ')
+emprego00 = input('Digite seu emprego dos sonhos: ')
+salario = int(input('Digite seu salário: '))
+gasto = int(input('Digite seus gatos: '))
+
+conta = salario - gasto
+# Melhor forma de colocar no print as variaveis com as informação
+print(f'seu nome é: {nome00}, e seu emprego vai ser {emprego00} com um salario de {salario} ')
+
+
+#Isto é o básico
+nome = 'Eduardo'
+emprego = 'programador'
+idade = 22
+
+print(nome, emprego, idade)
+
