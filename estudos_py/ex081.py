8c14046de9410893743aaea74145e1f436b24ac4
Eduardo Umbelino <umbelino.eduardo2003@gmail.com>
2025-11-04 00:17:36 -0300

    uptate 1.6.1
    funcionalidades do def

---
+++ estudos_py/ex081.py
@@ -0,0 +1,12 @@
+def area(largura, comprimento):
+    print(f'A largura é :{largura}')
+    print(f'O comprimento é :{comprimeto}')
+    ar = largura * comprimento
+    print(f'A área de um terreno {largura} X {comprimento} é de {ar} metros quadrados')
+    
+print('     Controle de Terrenos')
+print('-'*30)
+largura = float(input('Digite a largula (m): '))
+comprimeto = float(input('Digite o comprimento (m): '))
+
+area(largura = largura,comprimento = comprimeto)
---
+++ estudos_py/ex082.py
@@ -0,0 +1,10 @@
+def escreva(tex):
+    tam = len(tex) + 4
+    print(f'~'*tam)
+    print(f'  {tex}')
+    print(f'~'*tam)
+
+escreva('Eduardo Medeiros')
+escreva('Estudos Pyton')
+escreva('Novo mundo')
+escreva('Programador')
