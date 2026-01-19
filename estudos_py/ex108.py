d97c6e5a0c3d83ed79cd7a95baa4269c24bcc925
Eduardo Umbelino <umbelino.eduardo2003@gmail.com>
2026-01-19 18:56:49 -0300

    uptate 1.8.6
    Revisando, resolvendo problemas

--- estudos_py/ex108.py
+++ estudos_py/ex108.py
@@ -17,3 +17,40 @@ texto2 = set(input("Texto 2: ").lower().split())
 comuns = texto1.intersection(texto2) 
 
 print(f"Palavras em comum: {comuns}") 
+# outra atividade
+permissoes_principais = set(input("Permissões principais: ").strip().lower().split(',')) 
+
+permissoes_solicitadas = set(input("Permissões solicitadas: ").strip().lower().split(',')) 
+
+for i in range(len(permissoes_principais)):  
+
+    permissoes_principais[i] = permissoes_principais[i].strip() 
+
+for i in range(len(permissoes_solicitadas)):  
+
+    permissoes_solicitadas[i] = permissoes_solicitadas[i].strip() 
+
+eh_subconjunto = permissoes_solicitadas.issubset(permissoes_principais) 
+
+if eh_subconjunto:  # For verdade =
+
+    print("As permissões solicitadas fazem parte das permissões principais.")  
+
+else:  # se não
+
+    print("As permissões solicitadas não fazem parte das permissões principais.")
+
+    
+equipe_a = {"planejar reunião", "revisar documento", "testar sistema"}  
+
+equipe_b = {"testar sistema", "implementar funcionalidade", "corrigir bug"}  
+
+tarefas_combinadas = equipe_a.union(equipe_b) 
+
+tarefa_remover = input("Tarefa a ser removida: ").lower()  
+
+if tarefa_remover in tarefas_combinadas:  
+
+    tarefas_combinadas.remove(tarefa_remover) 
+
+print(f"Tarefas finais: {tarefas_combinadas}")  
